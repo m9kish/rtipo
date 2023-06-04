9952c1d7425663876ef7a0f3b2b4224c207b53d4
m9kish <tvo20000@yandex.ru>
2023-06-04 17:14:17 +0500

    init

---
+++ rtipo/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace rtipo
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            dataGridView1 = new DataGridView();
+            menuStrip1 = new MenuStrip();
+            toolStripMenuItem1 = new ToolStripMenuItem();
+            животныеToolStripMenuItem = new ToolStripMenuItem();
+            организацииToolStripMenuItem = new ToolStripMenuItem();
+            контрактыToolStripMenuItem = new ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            menuStrip1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(12, 27);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowTemplate.Height = 25;
+            dataGridView1.Size = new Size(638, 397);
+            dataGridView1.TabIndex = 0;
+            dataGridView1.CellContentClick += dataGridView1_CellContentClick;
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.Items.AddRange(new ToolStripItem[] { toolStripMenuItem1 });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Size = new Size(800, 24);
+            menuStrip1.TabIndex = 1;
+            menuStrip1.Text = "menuStrip1";
+            // 
+            // toolStripMenuItem1
+            // 
+            toolStripMenuItem1.DropDownItems.AddRange(new ToolStripItem[] { животныеToolStripMenuItem, организацииToolStripMenuItem, контрактыToolStripMenuItem });
+            toolStripMenuItem1.Name = "toolStripMenuItem1";
+            toolStripMenuItem1.Size = new Size(65, 20);
+            toolStripMenuItem1.Text = "Реестры";
+            toolStripMenuItem1.Click += toolStripMenuItem1_Click;
+            // 
+            // животныеToolStripMenuItem
+            // 
+            животныеToolStripMenuItem.Name = "животныеToolStripMenuItem";
+            животныеToolStripMenuItem.Size = new Size(180, 22);
+            животныеToolStripMenuItem.Text = "Животные";
+            // 
+            // организацииToolStripMenuItem
+            // 
+            организацииToolStripMenuItem.Name = "организацииToolStripMenuItem";
+            организацииToolStripMenuItem.Size = new Size(180, 22);
+            организацииToolStripMenuItem.Text = "Организации";
+            // 
+            // контрактыToolStripMenuItem
+            // 
+            контрактыToolStripMenuItem.Name = "контрактыToolStripMenuItem";
+            контрактыToolStripMenuItem.Size = new Size(180, 22);
+            контрактыToolStripMenuItem.Text = "Контракты";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(dataGridView1);
+            Controls.Add(menuStrip1);
+            MainMenuStrip = menuStrip1;
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private DataGridView dataGridView1;
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem toolStripMenuItem1;
+        private ToolStripMenuItem животныеToolStripMenuItem;
+        private ToolStripMenuItem организацииToolStripMenuItem;
+        private ToolStripMenuItem контрактыToolStripMenuItem;
+    }
+}
---
+++ rtipo/Form1.cs
@@ -0,0 +1,20 @@
+namespace rtipo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void toolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
